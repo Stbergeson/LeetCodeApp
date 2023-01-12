b4f780ad2705476e1aa7f4de8741e439d7737fc0
Stbergeson <sbergeson1@gmail.com>
2023-01-12 05:51:31 -0600

    Add project files.

---
+++ LeetCode.Tests/ProblemsTests/Tests.cs
@@ -0,0 +1,34 @@
+﻿using LeetCode.Problems;
+
+namespace LeetCode.Tests.ProblemsTests
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(false, new int[] { 1, 2, 3, 4 })]
+        public void ContainsDuplicate_ShouldReturnTrueIfDupeOtherwiseFalse(bool expected, int[] nums)
+        {
+            //arrange
+
+            //act
+            bool actual = _217.ContainsDuplicate(nums);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(true, "anagram", "nagaram")]
+        [InlineData(false, "rat", "car")]
+        public void IsAnagram_ShouldReturnTrueIfAnagram(bool expected, string orginalString, string anagram)
+        {
+            //arrange
+
+            //act
+            bool actual = _242.IsAnagram(orginalString, anagram);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ LeetCode/Problems/solutions.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Problems;
+
+internal static class _217
+{
+    internal static bool ContainsDuplicate(int[] nums)
+    {
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+internal static class _242
+{
+    internal static bool IsAnagram(string s, string t)
+    {
+        Dictionary<char, int> alphabet = new Dictionary<char, int>();
+
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            alphabet.Add(c, 0);
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            alphabet[s[i]]++;
+        }
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            alphabet[t[i]]--;
+        }
+
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            if (alphabet[c] != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
