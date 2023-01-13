50a4cf35ec7b31aa5dde2517c6d57c5f61a26d58
Stbergeson <sbergeson1@gmail.com>
2023-01-13 06:05:16 -0600

    Problem 49 - functional but slow

--- LeetCode.Tests/ProblemsTests/Tests.cs
+++ LeetCode.Tests/ProblemsTests/Tests.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCode.Problems;
+using System.Runtime.CompilerServices;
 
 namespace LeetCode.Tests.ProblemsTests
 {
@@ -32,15 +33,39 @@ namespace LeetCode.Tests.ProblemsTests
         }
 
         [Theory]
-        [InlineData(new int[] {0, 1}, new int[] {2, 7, 11, 15}, 9)]
-        [InlineData(new int[] {1, 2}, new int[] {3, 2, 4}, 6)]
-        [InlineData(new int[] {0, 1}, new int[] {3, 3}, 6)]
+        [InlineData(new int[] { 0, 1 }, new int[] { 2, 7, 11, 15 }, 9)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 3, 2, 4 }, 6)]
+        [InlineData(new int[] { 0, 1 }, new int[] { 3, 3 }, 6)]
         public void TwoSum_ReturnTwoIndiceValuesThatEqualTarget(int[] expected, int[] nums, int target)
         {
             //arrange
 
             //act
             int[] actual = _1.TwoSum(nums, target);
+
+            //assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        public static IEnumerable<object[]> GroupAnagramTestData =>
+           new List<object[]>
+           {
+                new object[] { new List<IList<string>> { 
+                    new List<string> {"eat", "tea", "ate"},
+                    new List<string> {"tan", "nat"},
+                    new List<string> {"bat"}
+                } , new string[] { "eat", "tea", "tan", "ate", "nat", "bat" } }
+           };
+
+        [Theory]
+        [MemberData(nameof(GroupAnagramTestData))]
+        public void GroupAnagrams_ShouldReturnListOfAngagramsGroupedTogether(IList<IList<string>> expected, string[] strs)
+        {
+            //arrange
+            List<string> test = new(){ "123", "123" };
+            //act
+            IList<IList<string>> actual = _49.GroupAnagrams(strs);
 
             //assert
             Assert.Equal(expected, actual);
--- LeetCode/Problems/solutions.cs
+++ LeetCode/Problems/solutions.cs
@@ -26,6 +26,74 @@ internal static class _1
     }
 }
 
+internal static class _49
+{
+    /*
+     * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+     * An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+     * typically using all the original letters exactly once.
+     */
+    internal static IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        IList<IList<string>> result = new List<IList<string>>();
+        List <string> stringsArray = new List<string>(strs);
+        List<string> tempArray;
+        List<string> list;
+        while (stringsArray.Count > 0)
+        {
+            tempArray = new();
+            list = new()
+            {
+                stringsArray[0]
+            };
+            for (int i = 1; i < stringsArray.Count; i++)
+            {
+                if (IsAnagram(stringsArray[0], stringsArray[i]))
+                {
+                    list.Add(stringsArray[i]);
+                }
+                else
+                {
+                    tempArray.Add(stringsArray[i]);
+                }
+            }
+            result.Add(list);
+            stringsArray = tempArray;
+        }
+
+        return result;
+
+    }
+
+    internal static bool IsAnagram(string s, string t)
+    {
+        Dictionary<char, int> alphabet = new Dictionary<char, int>();
+
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            alphabet.Add(c, 0);
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            alphabet[s[i]]++;
+        }
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            alphabet[t[i]]--;
+        }
+
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            if (alphabet[c] != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
+
 internal static class _217
 {
     internal static bool ContainsDuplicate(int[] nums)
